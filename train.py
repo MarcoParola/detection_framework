ae434234bfeff299024add1ddf9d84017f7538e0
ImBadnick <fuma2@hotmail.it>
2023-03-31 12:51:22 +0200

    added detr option in train.py

--- train.py
+++ train.py
@@ -73,6 +73,30 @@ def train(cfg):
         trainer.resume_or_load(resume=False)
         trainer.train()
 
+    if cfg.model == "detr":
+        '''
+        main_path = os.path.join(cfg.project_path, "models", "detr", "main.py")
+        batch_size = 2
+        epochs = 10
+        num_classes = 3
+        dataset_file = "'coco'"
+        coco_path = os.path.join(cfg.project_path, "data", "coco")
+        output_dir = os.path.join(cfg.project_path, "outputs", "detr")
+        device = "'cpu'"
+        resume = "'https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth'" #detr-resnet50
+
+        exec_command = f"{main_path}  \
+                       --batch_size={batch_size} \
+                       --epochs={epochs} \
+                       --num_classes={num_classes} \
+                       --dataset_file={dataset_file} \
+                       --coco_path = {coco_path} \
+                       --output_dir = {output_dir} \
+                       --device = {device} \
+                       --resume = {resume}"
+
+        os.system("python " + exec_command)
+        '''
 
 if __name__ == '__main__':
     train()
